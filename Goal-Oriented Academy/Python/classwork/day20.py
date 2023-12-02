c0561ed8f74a521ce8e06bcc52b230752779b18b
MateGiorgelashvili <cyanz0901@gmail.com>
2023-12-03 01:36:14 +0400

    new commit

---
+++ Goal-Oriented Academy/Python/classwork/day20.py
@@ -0,0 +1,7 @@
+# def abbrev_name(name):
+#     words = name.split()
+#     first_letter = words[0][0]
+#     second_letter = words[1][0]
+#     return first_letter, second_letter
+# print(abbrev_name("Sandro Zabakhidze"))
+  
---
+++ Goal-Oriented Academy/Python/homework/day20.py
@@ -0,0 +1,41 @@
+#HOMEWORK
+#1
+# def find_needle(haystack):
+#     index = haystack.index("needle")
+#     if "needle" in haystack:
+#         return "found the needle at position {}".format(index)
+
+#2
+# def make_upper_case(s):
+#     return s.upper()
+
+#3
+# def sum_array(a):
+#     if not a:
+#         return 0
+#     else:
+#         result = sum(a)
+#         return result
+
+#4
+# def are_you_playing_banjo(name):
+#     if "r" in name[0]:
+#         return (name + " plays banjo")
+#     elif "R" in name[0]:
+#         return (name + " plays banjo")
+#     else: 
+#         return (name + " does not play banjo")
+
+#5
+# def invert(lst):
+#     my_arr = []
+#     for i in lst:
+#         my_arr.append(i * -1)
+#     return my_arr
+
+#6
+# def find_average(numbers):
+#     if not numbers:
+#         return 0
+#     else:
+#         return sum(numbers) / len(numbers)
